55a57039e414a72161a42967732f12f972c6d835
Criblle <97399458+Criblle@users.noreply.github.com>
2022-01-27 22:34:33 +0300

    Create Silion.py

---
+++ Silion.py
@@ -0,0 +1,42 @@
+import os
+import colorama
+import time
+from time import sleep
+from colorama import init, Fore
+init(autoreset=True)
+print()
+print()
+print(Fore.GREEN + "   ██████  ██▓ ██▓     ██▓ ▒█████   ███▄    █ ")
+print(Fore.GREEN + " ▒██    ▒ ▓██▒▓██▒    ▓██▒▒██▒  ██▒ ██ ▀█   █")
+print(Fore.GREEN + " ░ ▓██▄   ▒██▒▒██░    ▒██▒▒██░  ██▒▓██  ▀█ ██▒")
+print(Fore.GREEN + "   ▒   ██▒░██░▒██░    ░██░▒██   ██░▓██▒  ▐▌██▒")
+print(Fore.GREEN + " ▒██████▒▒░██░░██████▒░██░░ ████▓▒░▒██░   ▓██░")
+print(Fore.GREEN + " ▒ ▒▓▒ ▒ ░░▓  ░ ▒░▓  ░░▓  ░ ▒░▒░▒░ ░ ▒░   ▒ ▒ ")
+print(Fore.GREEN + " ░ ░▒  ░ ░ ▒ ░░ ░ ▒  ░ ▒ ░  ░ ▒ ▒░ ░ ░░   ░ ▒░")
+print(Fore.GREEN + " ░  ░  ░   ▒ ░  ░ ░    ▒ ░░ ░ ░ ▒     ░   ░ ░")
+print(Fore.GREEN + "      ░   ░      ░  ░ ░      ░ ░           ░")
+print(" ====░=====░=====░==░=░======░=░===========░==")
+print("        Made by", Fore.CYAN + "Criblle                 ")
+print("        Updates:", Fore.CYAN + "https://t.me/tiktoksoft")
+print(" =============================================")
+print()
+print(Fore.YELLOW  + "                  Инструменты")
+print(Fore.MAGENTA  + " 1.[1000-7]")
+print()
+print(Fore.MAGENTA  + " '''''''''''''''''''''''''''''''''''''''''''' ")
+while True:
+    inp = int(input('<$> '))
+    if inp == 1:
+        print(Fore.GREEN + 'Вы выбрали [1000-7]')
+        sleep(2)
+        if os.sys.platform == "win32":
+            os.system("cls")
+        else:
+            os.system("clear")
+        import index.py
+    
+
+
+
+
+input()
